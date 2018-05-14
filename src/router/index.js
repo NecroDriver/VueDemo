b2664d614038dcf6abd4d21179868a76e61ec48a
mafh <1193482868@qq.com>
2018-05-14 18:48:58 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import indexPage from '../pages/index'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: indexPage
+    }
+  ]
+})
